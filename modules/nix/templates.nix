0b2708c2adc7d86b992527a16aa873511ec6ac85
areskill <areskul@areskul.com>
2026-07-03 23:26:01 +0200

    fix(den): every lib/nixos modules out of auto-import

--- modules/nix/templates.nix
+++ modules/nix/templates.nix
@@ -3,13 +3,13 @@
     defaultTemplate = templates.default;
     templates = {
       default = {
-        path = ../_templates/virshle-on-host;
+        path = ../_nixos/templates/virshle-on-host;
         description = ''
           A minimal nixos configuration flake for hosts running virshle.
         '';
       };
       vm = {
-        path = ../_templates/virshle-vm-compat;
+        path = ../_nixos/templates/virshle-vm-compat;
         description = ''
           A minimal nixos configuration flake for virshle VMs.
         '';
